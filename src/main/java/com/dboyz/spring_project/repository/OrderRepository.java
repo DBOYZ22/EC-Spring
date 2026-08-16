03a3b0ff073d4521892c48fbb6232819c5746cf9
DBOYZ22 <rightheart721@gmail.com>
2026-08-16 21:01:58 +0900

    Change interface OrderHistoryRepositoty to OrderRepository

--- src/main/java/com/dboyz/spring_project/repository/OrderRepository.java
+++ src/main/java/com/dboyz/spring_project/repository/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.dboyz.spring_project.entity.OrderHistory;
 
 @Repository
-public interface OrderHistoryRepository extends JpaRepository<OrderHistory, String> {
+public interface OrderRepository extends JpaRepository<OrderHistory, String> {
   // Add findByOrderNumber method to retrieve order history by order number
   Optional<OrderHistory> findByOrderNumber(String orderNumber);
 }
